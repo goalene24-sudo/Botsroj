f7c3a901dcc6d85bf90b998b50f8b89e85d86f3e
Abody <goalene24@gmail.com>
2025-08-22 21:17:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 from telethon import TelegramClient
 from datetime import datetime
 import config
+import logging
 
 StartTime = datetime.now()
 
 try:
     client = TelegramClient(None, config.API_ID, config.API_HASH)
 except Exception as e:
-    import logging
     logging.basicConfig(level=logging.INFO)
     logging.critical(f"!! خطأ فادح عند تهيئة البوت: {e}", exc_info=True)
     exit(1)
+    
