b08d5d5e644546654ed69c6f617a03c8d64ce770
Abody <goalene24@gmail.com>
2025-09-15 21:40:39 +0300

    Update shop.py

--- plugins/shop.py
+++ plugins/shop.py
@@ -9,7 +9,7 @@ import config
 from database import AsyncDBSession
 # --- استيراد الدوال المساعدة المحدثة ---
 from .utils import check_activation, add_points, get_or_create_user
-from .admin import get_or_create_chat
+from .utils import get_or_create_chat
 
 
 # تعريف أغراض المتجر
